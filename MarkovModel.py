5a7b41f0cb9d54c2915d850c53e102137575b762
jongmin yu <jmyu@gist.ac.kr>
2014-08-18 22:17:12 +0900

    markov model uploaded

---
+++ MarkovModel.py
@@ -0,0 +1,19 @@
+#__Author = 'Jongmin Yu' @ Ph.D Candidate @ GIST-MLV
+#__Laboratory of Machine Learning and Computer Vision
+#
+#--Title.Gaussian probability density function for image file
+#__data format  = numpy
+#
+
+from math import sqrt
+from parser import st2list
+import matplotlib
+
+from matplotlib import pyplot as plt
+import numpy as np
+import scipy
+import math
+import random
+import cv2
+
+class MarkovModel:
